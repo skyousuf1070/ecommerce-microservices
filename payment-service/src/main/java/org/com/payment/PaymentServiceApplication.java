8f25cf2ad5b5e82a72a3e556d450984a5fd8eb0e
yousuf <skyousuf1070@gmail.com>
2025-12-25 14:32:53 +0530

    Refactor service poms to inherit dependencies from root pom

--- payment-service/src/main/java/org/com/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/org/com/payment/PaymentServiceApplication.java
@@ -2,12 +2,13 @@ package org.com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
 
 }
